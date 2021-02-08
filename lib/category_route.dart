3ea7dab3dbd6fe457dc98bf4bb6ca3b5380af890
Aashish Tamsya <aashish.tamsya@gmail.com>
2021-02-08 18:48:41 +0530

    Add input and output views for converter screen

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -26,16 +26,54 @@ class _CategoryRouteState extends State<CategoryRoute> {
     'Currency'
   ];
 
-  static const _baseColor = <Color>[
-    Colors.teal,
-    Colors.orange,
-    Colors.pinkAccent,
-    Colors.blueAccent,
-    Colors.yellow,
-    Colors.greenAccent,
-    Colors.purpleAccent,
-    Colors.red
+  static const _baseColor = <ColorSwatch>[
+    ColorSwatch(0xFF6AB7A8, {
+      'highlight': Color(0xFF6AB7A8),
+      'splash': Color(0xFF0ABC9B),
+    }),
+    ColorSwatch(0xFFFFD28E, {
+      'highlight': Color(0xFFFFD28E),
+      'splash': Color(0xFFFFA41C),
+    }),
+    ColorSwatch(0xFFFFB7DE, {
+      'highlight': Color(0xFFFFB7DE),
+      'splash': Color(0xFFF94CBF),
+    }),
+    ColorSwatch(0xFF8899A8, {
+      'highlight': Color(0xFF8899A8),
+      'splash': Color(0xFFA9CAE8),
+    }),
+    ColorSwatch(0xFFEAD37E, {
+      'highlight': Color(0xFFEAD37E),
+      'splash': Color(0xFFFFE070),
+    }),
+    ColorSwatch(0xFF81A56F, {
+      'highlight': Color(0xFF81A56F),
+      'splash': Color(0xFF7CC159),
+    }),
+    ColorSwatch(0xFFD7C0E2, {
+      'highlight': Color(0xFFD7C0E2),
+      'splash': Color(0xFFCA90E5),
+    }),
+    ColorSwatch(0xFFCE9A9A, {
+      'highlight': Color(0xFFCE9A9A),
+      'splash': Color(0xFFF94D56),
+      'error': Color(0xFF912D2D),
+    }),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < _categoryNames.length; i++) {
+      _categories.add(Category(
+        color: _baseColor[i],
+        iconLocation: Icons.cake,
+        name: _categoryNames[i],
+        units: _retrieveUnitList(_categoryNames[i]),
+      ));
+    }
+  }
 
   Widget _buildCategoryWidgets() {
     return ListView.builder(
@@ -49,19 +87,6 @@ class _CategoryRouteState extends State<CategoryRoute> {
       i += 1;
       return Unit(name: '$categoryName Unit $i', conversion: i.toDouble());
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    for (var i = 0; i < _categoryNames.length; i++) {
-      _categories.add(Category(
-        color: _baseColor[i],
-        iconLocation: Icons.cake,
-        name: _categoryNames[i],
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
   }
 
   @override
